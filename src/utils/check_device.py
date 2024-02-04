b90271705c1d4951e02b86cd7e03dde35bfb346f
Miguel Ángel Manzano <mmanzanomo@gmail.com>
2024-02-04 06:25:33 +0100

    Updated check_device.py, make_dataset.py from utils

--- src/utils/check_device.py
+++ src/utils/check_device.py
@@ -1,12 +1,15 @@
 import torch
 
 
-# Check the device in use.
 def set_device():
+    """
+    Set and return the appropriate PyTorch device based on GPU availability.
+
+    Returns:
+        torch.device: PyTorch device, either 'cuda' if GPU is available or 'cpu' if not.
+    """
     if torch.cuda.is_available():
         device = torch.device('cuda')
     else:
         device = torch.device('cpu')
     return device
-
-
--- src/utils/make_dataset.py
+++ src/utils/make_dataset.py
@@ -3,13 +3,35 @@ import skimage.io as skio
 from pathlib import Path
 import shutil
 
-"""
-DATASET PATH
-"""
+
 def is_valid_path(path):
+    """
+    Check if the given path exists.
+
+    Args:
+        path (str): Path to be checked.
+
+    Returns:
+        bool: True if the path exists, False otherwise.
+    """
     return os.path.exists(path)
 
+
 def path_dataset(main_path, path_train="train", path_test="test"):
+    """
+    Generate and return paths for the training and testing datasets.
+
+    Args:
+        main_path (str): Main directory path.
+        path_train (str, optional): Subdirectory for the training dataset. Default is "train".
+        path_test (str, optional): Subdirectory for the testing dataset. Default is "test".
+
+    Returns:
+        Tuple[str, str]: Tuple containing paths for the training and testing datasets.
+
+    Prints:
+        str: Status message indicating whether the main path is valid or not.
+    """
     if is_valid_path(main_path):
         print(f"The path {main_path} is valid.")
     else:
@@ -20,10 +42,24 @@ def path_dataset(main_path, path_train="train", path_test="test"):
 
     return train_data_dir, test_data_dir
 
-"""
-LOAD DATA
-"""
+
 def load_chest_xray_data( data_directory ):
+    """
+    Load chest X-ray images and corresponding labels from the specified directory.
+
+    The function reads images with the '.jpeg' extension from each subdirectory in the given data directory.
+    It returns a tuple containing a list of images and their corresponding labels.
+
+    Args:
+        data_directory (str): Directory containing subdirectories for different classes.
+
+    Returns:
+        Tuple[List[np.ndarray], List[str]]: Tuple containing a list of images (as NumPy arrays) and their corresponding labels.
+
+    
+    Example:
+    - images, labels = load_chest_xray_data('/path/to/chest_xray_data')
+    """
     dirs = [ d for d in os.listdir( data_directory ) 
             if os.path.isdir( os.path.join( data_directory, d ) ) ]
 
@@ -41,10 +77,18 @@ def load_chest_xray_data( data_directory ):
     
     return images, labels
 
-"""
-REORGANIZE DATA
-"""
+
 def reorganize_pneumonia_data(main_dir):
+    """
+    Reorganize pneumonia data by creating separate folders for virus and bacteria images.
+
+    Args:
+        main_dir (str): Main directory containing 'train' and 'test' subdirectories.
+
+    The function reorganizes the pneumonia data within the 'train' and 'test' subsets.
+    It creates separate folders for 'PNEUMONIA_VIRUS' and 'PNEUMONIA_BACTERIA', moves virus images
+    to the new folder, and renames the 'PNEUMONIA' folder to 'PNEUMONIA_BACTERIA'.
+    """
     for subset in ['train', 'test']:
         pneumonia_folder = os.path.join(main_dir, subset, 'PNEUMONIA')
         virus_folder = os.path.join(main_dir, subset, 'PNEUMONIA_VIRUS')
